4db5f7be73bf467819dfc8db4135c80747839280
chuanriceeeeee <19033574145@163.com>
2026-03-04 11:52:51 +0800

    lesson3_3_4

--- _2/Project3_hidden_faces_romoval/Project3_hidden_faces_romoval/my_geometry.h
+++ _2/Project3_hidden_faces_romoval/Project3_hidden_faces_romoval/my_geometry.h
@@ -11,3 +11,27 @@ struct vec
 	double operator[](const int i) const { assert(i >= 0 && i < n); return data[n]; }
 };
 
+template <int n> 
+std::ostream& operator<< (std::ostream out, const vec<n>& v)
+{
+	for (int i = 0; i < n; i++) out << v[i] << " ";
+	return out;
+}
+
+template <> struct vec<3>
+{
+
+	double x = 0, y = 0, z = 0;
+	
+	vec() = default;
+
+	double& operator[](const int i) { assert(i >= 0 && i < 3); i ? ((i == 1) ? y : z) : x; }
+	double operator[](const int i) const { assert(i >= 0 && i < 3); i ? ((i == 1) ? y : z) : x;}
+
+	vec<3> operator+(vec<3> & v)
+	{
+		return vec<3>(x + v.x, y + v.y, z + v.z);
+	}
+};
+
+template <typename t>
