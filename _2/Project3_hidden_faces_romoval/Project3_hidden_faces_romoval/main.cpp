ce313a19d422ec011da1f31f74260c1578784903
chuanriceeeeee <19033574145@163.com>
2026-03-07 11:57:34 +0800

    better_camera_3_7

--- _2/Project3_hidden_faces_romoval/Project3_hidden_faces_romoval/main.cpp
+++ _2/Project3_hidden_faces_romoval/Project3_hidden_faces_romoval/main.cpp
@@ -7,6 +7,7 @@
 #include "geometry.h"
 #include <algorithm>
 #include <cmath>
+#include <Eigen/Dense>
 
 constexpr TGAColor white = { 255, 255, 255, 255 }; // attention, BGRA order
 constexpr TGAColor green = { 0, 255,   0, 255 };
@@ -16,93 +17,14 @@ constexpr TGAColor yellow = { 0, 200, 255, 255 };
 
 constexpr int width = 800;
 constexpr int height = 800;
+constexpr double focal_length = 3;
 
-
-void line(int xa, int ya, int xb, int yb, TGAImage& framebuffer, TGAColor color)
-{
-	bool steep = std::abs(xb - xa) < std::abs(yb - ya);//mistake 3 std::(yb-ya) mistake 4 > <
-	if (steep)
-	{
-		std::swap(xa, ya);
-		std::swap(xb, yb);
-	}
-	//transpose
-	if (xa > xb)
-	{
-		std::swap(xa, xb);
-		std::swap(ya, yb);
-	}
-	int temp = std::abs(yb - ya);//mistake 1
-	int y = ya;
-	int ierror = 0;
-	for (int x = xa; x <= xb; x++)
-	{//1 0 0 + 
-		ierror += temp * 2;
-		if (steep)
-			framebuffer.set(y, x, color);
-		else
-			framebuffer.set(x, y, color);
-		if (ierror >= xb - xa)
-		{
-			ierror -= 2 * (xb - xa);//mistake 2
-			y += yb > ya ? 1 : (-1);
-		}
-	}
-}
-
-void triangle_out(int xa, int ya, int xb, int yb, int xc, int yc, TGAImage& framebuffer, TGAColor color) {
-	if (yc > yb)
-	{
-		std::swap(xc, xb);
-		std::swap(yc, yb);
-	}
-	if (yb > ya)
-	{
-		std::swap(xa, xb);
-		std::swap(ya, yb);
-	}
-	if (yc > yb)
-	{
-		std::swap(xc, xb);
-		std::swap(yc, yb);
-	}
-
-	int total_height = (ya - yc);
-
-	if (xb != xc)
-	{
-		int segment_height = (yb - yc);
-		int x1 = xa;
-		int x2 = xa;
-		for (int y = ya; y >= yb; y--)
-		{
-			x1 = xa + (xb - xa) * (ya - y) / static_cast<float>(total_height - segment_height);
-			x2 = xa + (xc - xa) * (ya - y) / static_cast<float>(total_height);
-			for (int x = std::min(x1, x2); x <= std::max(x1, x2); x++)
-			{
-				framebuffer.set(x, y, color);
-			}
-		}
-		for (int y = yc; y <= yb; y++)
-		{
-			x1 = xc + (xb - xc) * (y - yc) / static_cast<float>(segment_height);
-			x2 = xc + (xa - xc) * (y - yc) / static_cast<float>(total_height);
-			for (int x = std::min(x1, x2); x <= std::max(x1, x2); x++)
-			{
-				framebuffer.set(x, y, color);
-			}
-		}
-	}
-
-
-}
 float triangle_area(int xa, int  ya, int xb, int yb, int xc, int yc)
 {
 	return .5 * ((yb - ya) * (xb + xa) + (ya - yc) * (xa + xc) + (yc - yb) * (xc + xb));
 }
 
 
-//final version
 void triangle(int xa, int ya, int za, int xb, int yb, int zb, int xc, int yc, int zc, TGAImage& framebuffer, TGAColor color)
 {
 	int boxmax_x = std::max(std::max(xa, xb), xc);
@@ -142,42 +64,51 @@ void triangle(int xa, int ya, int za, int xb, int yb, int zb, int xc, int yc, in
 }
 
 
-vec4 project(vec4 vert)
+constexpr mat<4, 4> project_matrix = { {
+		{width / 2.0, 0, 0, width / 2.0},
+		{0, height / 2.0, 0, height / 2.0},
+		{0, 0, 255. / 2.0, 255. / 2.0},
+		{0, 0, 0, 1}
+		} };
+
+constexpr double ang = 45.0 * M_PI / 180.0;
+mat<4, 4> rotate_matrix = { {
+	{std::cos(ang), 0, std::sin(ang), 0 },
+	{0, 1, 0, 0 },
+	{ -(std::sin(ang)), 0, std::cos(ang), 0 },
+	{ 0, 0, 0, 1 }
+	} };
+
+constexpr mat<4, 4> Pers = { {
+		{1.0, 0, 0, 0},
+		{0, 1.0, 0, 0},
+		{0, 0, 1.0, 0},
+		{0, 0, -1.0 / (focal_length), 1.0}
+		} };
+
+vec4 project(const vec4 vert)
 {
-	return vec4(
-		(vert.x + 1) * width / 2,
-		(vert.y + 1) * height / 2,
-		(vert.z + 1) / 2.0 * 255.0,
-		0
-	);
-}
-vec4 perspective(vec4 vert)
-{
-	constexpr double t = 3.0 ;
-	return vert / ( 1.0 - vert.z / t);
+	return project_matrix * vert ;
 }
 
-vec4 rot_y(vec4 vert)
-{
-	constexpr double ang = 45.0 * M_PI / 180.0;
 
-	mat<4, 4> R = {{
-		{std::cos(ang), 0, std::sin(ang), 0 }, 
-		{0, 1, 0, 0 }, 
-		{ -(std::sin(ang)), 0, std::cos(ang), 0 }, 
-		{ 0, 0, 0, 1 }
-		}};
-	return R * vert;
+vec4 perspective(const vec4 vert)
+{
+	vec4 temp = Pers * vert;
+	return temp / temp . w;
 }
+
+vec4 rot_y(const vec4 vert)
+{
+	return rotate_matrix * vert;
+}
+
+
 
 
 int main(int argc, char** argv) {
 	TGAImage framebuffer_model(width, height, TGAImage::GRAYSCALE);
-	/*TGAImage framebuffer(width, height, TGAImage::RGB);
-	triangle(7, 45, 35, 100, 45, 60, framebuffer, red);
-	triangle(120, 35, 90, 5, 45, 110, framebuffer, white);
-	triangle(115, 83, 80, 90, 85, 120, framebuffer, green);
-	*/
+
 	Model* model = new Model("./models/diablo3_pose.obj");
 	auto currrent = std::chrono::steady_clock::now();
 	for (int i = 0; i < model->nfaces(); i++)
