ec18520c3d785647bb922f0aefe1711f650fabea
chuanriceeeeee <19033574145@163.com>
2026-03-13 20:45:46 +0800

    backup_3_13

--- _3_shader/Project3/Project3/main.cpp
+++ _3_shader/Project3/Project3/main.cpp
@@ -1,6 +1,68 @@
-#pragma once
-#include "tgaimage.h"
-#include "geometry.h"
+#include <cstdlib>
+#include "our_gl.h"
+#include "model.h"
+#include <cmath>
 
-void line(const int xa, const int ya, const int xb, const int yb, TGAImage& framebuffer, TGAColor color);
-void rasterize()
+extern mat<4, 4> ModelView, Perspective; // "OpenGL" state matrices and
+extern std::vector<double> zbuffer;     // the depth buffer
+
+struct RandomShader : IShader // Inheritate
+{
+    const Model& model;
+    TGAColor color = { };
+    vec3 tri[3];
+
+    RandomShader(const Model& m) : model(m)//输入模型时使用model初始化提取m模型信息
+    {}
+
+    virtual vec4 vertex(const int face, const int vert)
+    {
+        vec3 v = model.vert(face, vert);  //face的vert点向量vec4
+        vec4 gl_Position = ModelView * vec4{ v.x,v.y,v.z,1. };
+        tri[vert] = gl_Position.xyz();
+        return Perspective * gl_Position;
+    }
+
+    virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const
+    {
+        return { false, color };
+    }
+};
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
+        return 1;
+    }
+
+    constexpr int width = 800;      // output image size
+    constexpr int height = 800;
+    constexpr vec3    eye{ -1, 0, 2 }; // camera position
+    constexpr vec3 center{ 0, 0, 0 }; // camera direction
+    constexpr vec3     up{ 0, 1, 0 }; // camera up vector
+
+    (eye, center, up);                                   // build the ModelView   matrix
+    init_perspective(norm(eye - center));                        // build the Perspective matrix
+    init_viewport(width / 16, height / 16, width * 7 / 8, height * 7 / 8); // build the Viewport    matrix
+    init_zbuffer(width, height);
+    TGAImage framebuffer(width, height, TGAImage::RGB, { 177, 195, 209, 255 });
+
+    for (int m = 1; m < argc; m++) {                    // iterate through all input objects
+        Model model(argv[m]);                       // load the data
+        RandomShader shader(model);
+        for (int f = 0; f < model.nfaces(); f++) {      // iterate through all facets
+            shader.color = { 
+                static_cast<unsigned char>(std::rand() % 255), 
+                static_cast<unsigned char>(std::rand() % 255), 
+                static_cast<unsigned char>(std::rand() % 255), 
+                255 };
+            Triangle clip = { shader.vertex(f, 0),  // assemble the primitive
+                              shader.vertex(f, 1),
+                              shader.vertex(f, 2) };
+            rasterize(clip, shader, framebuffer);   // rasterize the primitive
+        }
+    }
+
+    framebuffer.write_tga_file("framebuffer.tga");
+    return 0;
+}
---
+++ _3_shader/Project3/Project3/our_gl.cpp
@@ -0,0 +1,94 @@
+#include "our_gl.h"
+
+mat<4, 4> ModelView, Viewport, Perspective;
+std::vector<double> zbuffer;
+
+void init_modelview(const vec3 eye, const vec3 center, const vec3 up) {
+	vec3 n = normalized(eye - center);
+	vec3 l = normalized(cross(up, n));
+	vec3 m = normalized(cross(n, l));
+	ModelView = mat<4, 4>{ {{l.x,l.y,l.z,0}, {m.x,m.y,m.z,0}, {n.x,n.y,n.z,0}, {0,0,0,1}} } *
+		mat<4, 4>{{{1, 0, 0, -center.x}, { 0,1,0,-center.y }, { 0,0,1,-center.z }, { 0,0,0,1 }}};
+}
+
+void init_perspective(const double f) {
+	Perspective = { {{1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0, -1 / f,1}} };
+}
+
+void init_viewport(const int x, const int y, const int w, const int h) {
+	Viewport = { {{w / 2., 0, 0, x + w / 2.}, {0, h / 2., 0, y + h / 2.}, {0,0,255. / 2.0 , 255. / 2.0}, {0,0,0,1}} };
+}
+
+void init_zbuffer(const int width, const int height)
+{
+	zbuffer = std::vector(width * height, -1000.);
+}
+
+float triangle_area(int xa, int  ya, int xb, int yb, int xc, int yc)
+{
+	return .5 * ((yb - ya) * (xb + xa) + (ya - yc) * (xa + xc) + (yc - yb) * (xc + xb));
+}
+
+void rasterize(const Triangle & clip, const IShader& shader, TGAImage& framebuffer )
+{
+	vec4 ndc[3] = { clip[0] / clip[0].w, clip[1] / clip[1].w , clip[2] / clip[2].w };
+	vec2 screen[3] = { (Viewport * ndc[0]).xy(),(Viewport * ndc[1]).xy(),(Viewport * ndc[2]).xy()) };
+
+	mat<3, 3> ABC = { {
+		{screen[0].x,screen[0].y,1},
+		{screen[1].x,screen[1].y,1},
+		{screen[2].x,screen[2].y,1},
+		} };
+	if (ABC.det() < 1) return;
+
+
+	
+
+	//int boxmin_x = clip[0].x;
+	//for (int i = 1; i < 3; i++)
+	//	boxmin_x = std::min(boxmin_x, static_cast<int>(clip[i].x));
+
+	//int boxmin_y = clip[0].y;
+	//for (int i = 1; i < 3; i++)
+	//	boxmin_y = std::min(boxmin_y, static_cast<int>(clip[i].y));
+
+	//int boxmax_x = clip[0].x;
+	//for (int i = 1; i < 3; i++)
+	//	boxmax_x = std::max(boxmax_x, static_cast<int>(clip[i].x));
+
+	//int boxmax_y = clip[0].y;
+	//for (int i = 1; i < 3; i++)
+	//	boxmax_y = std::max(boxmax_y, static_cast<int>(clip[i].y));
+
+	//int xa = clip[0].x, ya = clip[0].y, za = clip[0].z,
+	//	xb = clip[1].x, yb = clip[1].y, zb = clip[1].z,
+	//	xc = clip[2].x, yc = clip[2].y, zc = clip[2].z;
+
+	//double total_area = triangle_area(xa, ya, xb, yb, xc, yc);
+	//if (total_area < 0.5)
+	//	return;
+	////#pragma omp parallel for
+	//for (int x = boxmin_x; x <= boxmax_x; x++)
+	//{
+	//	for (int y = boxmin_y; y <= boxmax_y; y++)
+	//	{
+	//		double alpha = triangle_area(xa, ya, xb, yb, x, y) / total_area;
+	//		double beta = triangle_area(xb, yb, xc, yc, x, y) / total_area;
+	//		double gamma = triangle_area(xc, yc, xa, ya, x, y) / total_area;
+	//		if (alpha < 0 || beta < 0 || gamma < 0)
+	//			continue;
+
+	//		//if (alpha < 0.1 || beta < 0.1 || gamma < 0.1)
+	//		{
+	//			double z_double = zc * alpha + za * beta + zb * gamma;
+	//			double b = gamma * 255.0;
+	//			double g = beta * 255.0;
+	//			double r = alpha * 255.0;
+	//			unsigned char z = static_cast<unsigned char>(z_double);
+	//			shader.color = { static_cast<unsigned char>(b),static_cast<unsigned char>(g),static_cast<unsigned char>(r),255 };
+	//			if (z <= framebuffer.get(x, y)[0]) continue; //记录(x,y)像素点上的最大深度
+	//			framebuffer.set(x, y, { z });
+	//		}
+	//	}
+	//}
+}
