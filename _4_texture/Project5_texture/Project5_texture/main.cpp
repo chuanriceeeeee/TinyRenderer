9691c82dd9b04abf0d6f8e8ab686a9d1000d079f
chuanriceeeeee <19033574145@163.com>
2026-03-17 17:04:43 +0800

    3-17-texture

--- _4_texture/Project5_texture/Project5_texture/main.cpp
+++ _4_texture/Project5_texture/Project5_texture/main.cpp
@@ -33,88 +33,115 @@ extern mat<4, 4> ModelView, Perspective; // "OpenGL" state matrices and
 extern std::vector<double> zbuffer;     // the depth buffer
 
 
-struct PhongShader : IShader // Inheritate
+struct  BlingPhongShader : IShader // Inheritate
 {
-    const Model& model;
-    TGAColor color = { };
-    vec3 l;
-    vec3 eye_real_position;
-    vec3 tri[3];
-    vec3 norm[3];
-    PhongShader(const vec3 light, const Model& m,const vec3 eye) : model(m)//输入模型时使用model初始化提取m模型信息
-    {
-        l = normalized((ModelView * vec4{ light.x, light.y, light.z, 0. }).xyz());
-        eye_real_position = (ModelView * vec4{ eye.x,eye.y,eye.z,1. }).xyz();//无需归一化, eye的坐标经过平移旋转后的位置，不使用Perspective防止缩放后变形
-    }
+	const Model& model;
+	TGAColor color = { };
+	vec3 l;
+	vec3 eye_real_position;
+	vec3 tri[3];
+	vec3 norm[3];
+	vec2 uv[3];
 
-    virtual vec4 vertex(const int face, const int vert)
-    {
-        vec3 v = model.vert(face, vert);  //face的vert点向量vec4
-        vec4 gl_Position = ModelView * vec4{ v.x,v.y,v.z,1. }; //相对于{0，0，0}位置相机的视线方向
-        tri[vert] = gl_Position.xyz();
-        return Perspective * gl_Position;
-    }
+	TGAImage diffusemap;
+	TGAImage specularmap;
 
-    void normal(const int face, const int vert)
-    {
-        vec4 n = model.normal(face, vert);
-        vec4 normal_direction = ModelView.invert_transpose() * vec4{ n.x,n.y,n.z,0. }; //防止法线经过缩放后不再垂直
-        norm[vert] = normal_direction.xyz();
-    }
+	BlingPhongShader(const vec3 light, const Model& m, const vec3 eye) : model(m)//输入模型时使用model初始化提取m模型信息
+	{
+		l = normalized((ModelView * vec4{ light.x, light.y, light.z, 0. }).xyz());
+		eye_real_position = (Perspective * ModelView * vec4{ eye.x,eye.y,eye.z,1. }).xyz();//无需归一化, eye的坐标经过平移旋转后的位置，不使用Perspective防止缩放后变形
 
-    virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const
-    {
-        TGAColor gl_FragColor{ 255,255,255,255 };
-        vec3 frag_pos = tri[0] * bar.x + tri[1] * bar.y + tri[2] * bar.z;//实际坐标
+	}
 
-        vec3 n = normalized(bar.x * norm[0] + bar.y * norm[1] + bar.z * norm[2]);
-        vec3 r = normalized(n * (n * l) * 2 - l);       
-        vec3 v = normalized(eye_real_position - frag_pos);//已在rasterize中normalized
-        double ambient = .3; 
-        double diff = std::max(0., n * l); //漫反射强度，法线和光线的距离，越近越亮
-        double spec = 0.;
-        // 修复1：只有当前片段朝向光源（漫反射大于0）时，才会有高光！防止背面高光透射。
-        if (diff > 0.0) {
-            vec3 h = normalized(l + v);
-            spec = std::pow(std::max(h * n, 0.0), 64);
-        }
-        for (int channel : {0, 1, 2})
-            gl_FragColor[channel] *= std::min(1., ambient + .4 * diff + .9 * spec); // .4 .9 即为漫反射系数 高光反射系数 
-        return { false, gl_FragColor };
-    }
+	virtual vec4 vertex(const int face, const int vert)
+	{
+		vec3 v = model.vert(face, vert);  //face的vert点向量vec4
+		vec4 gl_Position = ModelView * vec4{ v.x,v.y,v.z,1. }; //相对于{0，0，0}位置相机的视线方向
+		tri[vert] = gl_Position.xyz();//读取当前点的转换后实际3d位置
+		vec2 uv_value = model.uv(face, vert);//读取uv值
+		uv[vert] = uv_value;
+
+		diffusemap = model.diffuse();
+		specularmap = model.specular();
+
+		return Perspective * gl_Position;
+	}
+
+	void normal(const int face, const int vert)
+	{
+		vec4 n = model.normal(face, vert);
+		vec4 normal_direction = ModelView.invert_transpose() * vec4 { n.x, n.y, n.z, 0. }; //防止法线经过缩放后不再垂直
+		norm[vert] = normal_direction.xyz();
+	}
+
+	virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const
+	{
+		TGAColor gl_FragColor{ 255,255,255,255 };
+		vec3 frag_pos = tri[0] * bar.x + tri[1] * bar.y + tri[2] * bar.z;//实际坐标
+		vec2 frag_uv = uv[0] * bar.x + uv[1] * bar.y + uv[2] * bar.z;
+
+		//vec3 n_out = normalized(bar.x * norm[0] + bar.y * norm[1] + bar.z * norm[2]);
+		vec3 n = model.normal(frag_uv).xyz();
+		vec3 r = normalized(n * (n * l) * 2 - l);
+		vec3 v = normalized(eye_real_position - frag_pos);//已在rasterize中normalized
+
+		TGAColor temp = diffusemap.get(frag_uv.x * diffusemap.width(), frag_uv.y * diffusemap.height()); // 此处需要乘以对应map的宽度和高度，否则无法得到正确rbg值
+		TGAColor temp2 = specularmap.get(frag_uv.x * specularmap.width(), frag_uv.y * specularmap.height());
+		double ambient = .6;
+		double diff = std::max(0., n * l); //漫反射强度，法线和光线的距离，越近越亮
+		double spec = 0.;
+		if (diff > 0.0) { // 只有当前片段朝向光源（漫反射大于0）时，才会有高光！防止背面高光透射。
+			vec3 h = normalized(l + v); //Bling-Phong shader 半程向量 通过原光线夹角和视线夹角的半程向量和法线的夹角来决定反射光线和视线的夹角是否有差
+			spec = std::pow(std::max(h * n, 0.0), 64);
+		}
+		double final_light = std::min(1., ambient + .4 * diff + .9 * spec);
+
+		for (int channel : {0, 1, 2})
+			gl_FragColor[channel] = temp[channel] * final_light; // .4 .9 即为漫反射系数 高光反射系数 
+		return { false, gl_FragColor };
+	}
+
+
 };
 
 int main(int argc, char** argv) {
-    constexpr int width = 800;      // output image size
-    constexpr int height = 800;
-    constexpr vec3    eye{ -1, 0, 2 }; // camera position
-    constexpr vec3 center{ 0, 0, 0 }; // camera direction
-    constexpr vec3     up{ 0, 1, 0 }; // camera up vector
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " model/african_head/african_head.obj" << std::endl;
+		return 1;
+	}
+	constexpr int width = 800;      // output image size
+	constexpr int height = 800;
+	constexpr vec3    eye{ -1, 0, 1 }; // camera position
+	constexpr vec3 center{ 0, 0, 0 }; // camera direction
+	constexpr vec3     up{ 0, 1, 0 }; // camera up vector
 
-    init_modelview(eye, center, up);                                   // build the ModelView   matrix
-    init_perspective(norm(eye - center));                        // build the Perspective matrix
-    init_viewport(width / 16, height / 16, width * 7 / 8, height * 7 / 8); // build the Viewport    matrix
-    init_zbuffer(width, height);
-    TGAImage framebuffer(width, height, TGAImage::RGB);
+	init_modelview(eye, center, up);                                   // build the ModelView   matrix
+	init_perspective(norm(eye - center));                        // build the Perspective matrix
+	init_viewport(width / 16, height / 16, width * 7 / 8, height * 7 / 8); // build the Viewport    matrix
+	init_zbuffer(width, height);
+	TGAImage framebuffer(width, height, TGAImage::RGB);
 
 
-    Model model("models/african_head/african_head.obj");                     // load the data
-    PhongShader shader({1,1,1},model,eye);
-        for (int f = 0; f < model.nfaces(); f++) {      // iterate through all facets
-            //shader.color = {
-            //    static_cast<unsigned char>(std::rand() % 255),
-            //    static_cast<unsigned char>(std::rand() % 255),
-            //    static_cast<unsigned char>(std::rand() % 255),
-            //    255 };
-                shader.normal(f, 0);
-                shader.normal(f, 1);
-                shader.normal(f, 2); //传入法线
+	for (int m = 0; m < argc; m++)
+	{
+		Model model(argv[m]);
+		BlingPhongShader shader({ 1,1,1 }, model, eye);
+		for (int f = 0; f < model.nfaces(); f++) {      // iterate through all facets
+			//shader.color = {
+			//    static_cast<unsigned char>(std::rand() % 255),
+			//    static_cast<unsigned char>(std::rand() % 255),
+			//    static_cast<unsigned char>(std::rand() % 255),
+			//    255 };
+			shader.normal(f, 0);
+			shader.normal(f, 1);
+			shader.normal(f, 2); //传入法线
 
-            Triangle clip = { shader.vertex(f, 0),  // assemble the primitive
-                              shader.vertex(f, 1),
-                              shader.vertex(f, 2) };
-            rasterize(clip, shader, framebuffer);   // rasterize the primitive
-        }
-    framebuffer.write_tga_file("framebuffer.tga");
-    return 0;
+			Triangle clip = { shader.vertex(f, 0),  // assemble the primitive
+							  shader.vertex(f, 1),
+							  shader.vertex(f, 2) };
+			rasterize(clip, shader, framebuffer);   // rasterize the primitive
+		}
+	}
+	framebuffer.write_tga_file("framebuffer.tga");
+	return 0;
 }
